27996991be98dcd75e2782b82a67014f27ff77e1
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2019-07-15 16:21:58 +0300

    sim-crit strucutre

---
+++ R/load-drat.R
@@ -0,0 +1,4 @@
+# library(drat)
+# addRepo("kvasilopoulos")
+# insertPackage("exuberdata_0.0.0.9000.tar.gz")
+# install.packages("exuberdata", repos = "https://kvasilopoulos.github.io/drat/", type = "source")
