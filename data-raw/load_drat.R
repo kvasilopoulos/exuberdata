ab3e604db18d8f445c49ff6523e1a5bd03814d33
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2020-03-06 16:15:10 +0000

    removed double commit in load_drat

--- data-raw/load_drat.R
+++ data-raw/load_drat.R
@@ -10,8 +10,8 @@ load_drat <- function(repo = "../drat") {
   
   # git2r::pull(drat_repo, ".")
   
-  git2r::add(drat_repo, ".")
-  git2r::commit(drat_repo, "Update Data")
+  # git2r::add(drat_repo, ".")
+  # git2r::commit(drat_repo, "Update Data")
   git2r::push(drat_repo, name = "origin", refspec = "refs/heads/gh-pages", 
               credentials = git2r::cred_token(), set_upstream = TRUE)
   on.exit(unlink(pkg))
