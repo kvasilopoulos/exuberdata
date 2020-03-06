f6549abc1f55f3a923224581963d4018129ae96f
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2020-03-06 16:13:14 +0000

    groom the package

--- data-raw/load_drat.R
+++ data-raw/load_drat.R
@@ -3,10 +3,12 @@
 load_drat <- function(repo = "../drat") {
   pkg <- devtools::build()
   drat::addRepo("kvasilopoulos")
-  drat::insertPackage(pkg, repodir = repo)
+  drat::insertPackage(pkg, repodir = repo, commit = TRUE)
   
   # commit and push the new version -----------------------------------------
   drat_repo <- git2r::repository(repo)
+  
+  # git2r::pull(drat_repo, ".")
   
   git2r::add(drat_repo, ".")
   git2r::commit(drat_repo, "Update Data")
