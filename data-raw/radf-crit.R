cc85d192d5c5e6eabaefa858efbb3fad26ec46ca
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2020-04-15 13:04:27 +0100

    remove opts in mc_cv

--- data-raw/radf-crit.R
+++ data-raw/radf-crit.R
@@ -1,23 +1,31 @@
 
+# install_github("kvasilopoulos/exuber@dev")
 library(exuber)
 options("exuber.parallel" = TRUE)
-options("exuber.ncores" = parallel::detectCores() - 1)
+options("exuber.ncores" = parallel::detectCores())
 options("exuber.show_progress" = TRUE)
 
 
 # Simulation -------------------------------------------------------------
-
 nn <- 2000
 radf_crit <- list()
-for (i in 701:nn) {
-  radf_crit[[i]] <-  exuber::mc_cv(i, seed = 123)
+for (i in 6:nn) {
+  radf_crit[[i]] <-  exuber::mc_cv(i, seed = 123, nrep = 2000)
   print(i)
 }
-names(radf_crit) <- c(paste0("_nan", 1:5), paste0("n", 6:nn))
-class(radf_crit) <- c("list", "crit")
+names(radf_crit2) <- c(paste0("_nan", 1:5), paste0("n", 6:nn))
+class(radf_crit2) <- c("list", "crit")
 
+# test -------------------------------------------------------------------
 
-# Store -------------------------------------------------------------------
+testthat::test_that("same results n = 100",{
+  all.equal( radf_crit2[[100]], mc_cv(701, nrep = 2000L, seed = 123))
+})
 
-usethis::use_data(radf_crit, overwrite = TRUE , compress = "xz")
-  
+testthat::test_that("same results n = 701",{
+  all.equal( radf_crit2[[701]], mc_cv(701, nrep = 2000L, seed = 123))
+})
+
+# store -------------------------------------------------------------------
+
+usethis::use_data(radf_crit2, overwrite = TRUE , compress = "xz")
