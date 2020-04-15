0ecabf3d8868257e93a3bb86bfaf601529a3ef7b
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2020-04-15 16:45:34 +0100

    updated data

--- data-raw/load_drat.R
+++ data-raw/load_drat.R
@@ -1,14 +1,14 @@
 push_local <- function(msg = NULL) {
   if (is.null(msg)) msg <- "updated data"
   local_repo <- git2r::repository()
-  git2r::add(local_repo)
+  git2r::add(local_repo, path = ".")
   git2r::commit(local_repo, message = msg)
-  git2::push(local_repo, name = "origin",
+  git2r::push(local_repo, name = "origin",
              credentials = git2r::cred_token(), set_upstream = TRUE)
 }
 
 load_drat <- function(repo = "../drat", msg) {
-  # Assure that you have update local repo before pushing to drat
+  # Assure that you have update local repo b,efore pushing to drat
   
   
   # Start Building
